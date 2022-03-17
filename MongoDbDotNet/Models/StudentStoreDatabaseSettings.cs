0b69f3aab19804e1953cabb9f6b177015d262b56
1251184-Robiul Hossain <rhrashal50@gmail.com>
2022-03-17 23:08:25 +0600

    initial upload

---
+++ MongoDbDotNet/Models/StudentStoreDatabaseSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MongoDbDotNet.Models
+{
+    public class StudentStoreDatabaseSettings : IStudentStoreDatabaseSettings
+    {
+        public string StudentCoursesCollectionName { get; set; } = String.Empty;
+        public string ConnectionString { get; set; } = String.Empty;
+        public string DatabaseName { get; set; } = String.Empty;
+    }
+}
---
+++ MongoDbDotNet/Service/IStudentService.cs
@@ -0,0 +1,17 @@
+﻿using MongoDbDotNet.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MongoDbDotNet.Service
+{
+    public interface IStudentService
+    {
+        List<Student> Get();
+        Student Get(string id);
+        Student Create(Student student);
+        void Update(string id, Student student);
+        void Remove(string id);
+    }
+}
